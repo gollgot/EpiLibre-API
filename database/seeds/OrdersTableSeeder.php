281a628e4a705ce0239c0c8414bbb4b5a85c6099
Loïc Dessaules <loic.dessaules@gmail.com>
2020-08-13 18:48:09 +0200

    Fix double val bug

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -14,7 +14,7 @@ class OrdersTableSeeder extends Seeder
     {
         DB::table('orders')->insert([
             'id' => 1,
-            'totalPrice' => 10.5,
+            'totalPrice' => doubleval(10.5),
             'created_at' => "2020-08-13 17:23:30",
             'updated_at' => "2020-08-13 17:23:30",
             'user_id' => 1
@@ -22,7 +22,7 @@ class OrdersTableSeeder extends Seeder
 
         DB::table('orders')->insert([
             'id' => 2,
-            'totalPrice' => 2.45,
+            'totalPrice' => doubleval(2.45),
             'created_at' => "2020-07-04 10:20:00",
             'updated_at' => "2020-07-04 10:20:00",
             'user_id' => 2
@@ -30,7 +30,7 @@ class OrdersTableSeeder extends Seeder
 
         DB::table('orders')->insert([
             'id' => 3,
-            'totalPrice' => 8.9,
+            'totalPrice' => doubleval(8.9),
             'created_at' => "2020-07-20 21:00:00",
             'updated_at' => "2020-07-20 21:00:00",
             'user_id' => 1
